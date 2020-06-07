17c2349048be81615b4bc8e2cac4cad8a7750713
Michael Sanders <mike.gamedev@gmail.com>
2020-06-07 12:55:58 -0700

    Enabled shadows on sprites.

---
+++ Assets/_Project/Scripts/EnableSpriteShadows.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableSpriteShadows : MonoBehaviour
+{
+    void Start()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer == null) throw new System.Exception("No renderer found.");
+
+        renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+        renderer.receiveShadows = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
