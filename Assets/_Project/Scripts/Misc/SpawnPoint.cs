1f73f6b95527366b7041a8797db7eb91ff04781a
Michael Sanders <mike.gamedev@gmail.com>
2020-06-13 16:08:05 -0700

    Added spawn points.

---
+++ Assets/_Project/Scripts/Misc/SpawnPoint.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace PixelCurio.AlteredTimeline
+{
+    public class SpawnPoint : MonoBehaviour
+    {
+        [SerializeField] private Alignment _spawnAlignment;
+        [SerializeField] private int _priority;
+        public enum Alignment { Right, Left }
+    }
+}
